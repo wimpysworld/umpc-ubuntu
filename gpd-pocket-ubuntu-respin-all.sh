1929cd3cc96eb73eb25946178126f4d2df407551
Martin Wimpress <code@flexion.org>
2019-04-08 23:54:24 +0100

    Update respin-all to include 18.10.

--- gpd-pocket-ubuntu-respin-all.sh
+++ gpd-pocket-ubuntu-respin-all.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-for ISO in ubuntu-mate-18.04.2-desktop-amd64.iso ubuntu-mate-19.04-beta-desktop-amd64.iso; do
+sudo true
+for ISO in ubuntu-mate-18.04.2-desktop-amd64.iso ubuntu-mate-18.10-desktop-amd64.iso ubuntu-mate-19.04-beta-desktop-amd64.iso; do
   for POCKET in gpd-pocket gpd-pocket2; do
     sudo ./gpd-pocket-ubuntu-respin.sh -d ${POCKET} ${ISO}
   done
