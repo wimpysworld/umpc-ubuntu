b46ee045c21edf3b3fa6f41086b98c91c5c62dd2
Martin Wimpress <code@flexion.org>
2019-04-08 13:12:44 +0100

    Add meta build script.

---
+++ gpd-pocket-ubuntu-respin-all.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+for ISO in ubuntu-mate-18.04.2-desktop-amd64.iso ubuntu-mate-19.04-beta-desktop-amd64.iso; do
+  for POCKET in gpd-pocket gpd-pocket2; do
+    sudo ./gpd-pocket-ubuntu-respin.sh -d ${POCKET} ${ISO}
+  done
+done
+
+for POCKET_ISO in *-gpd-pocket*.iso; do
+  ~/Scripts/mate/sign_image.sh ${POCKET_ISO}
+done
