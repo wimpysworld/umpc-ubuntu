e3239032d0c0918235cc033d7a7ab2d1f9680520
casainho <casainho@gmail.com>
2019-07-15 10:11:27 +0100

    Update gpd-pocket-ubuntu-respin.sh (#12)
    Correction on instructions to respin the ISO.

--- gpd-pocket-ubuntu-respin.sh
+++ gpd-pocket-ubuntu-respin.sh
@@ -10,7 +10,8 @@ function usage() {
     echo
     echo "OPTIONS"
     echo "    -d"
-    echo "        device modifications to apply to the iso image, can be 'gpd-pocket', 'gpd-pocket-2' or 'gpd-micropc'"
+    echo "        device modifications to apply to the iso image, can be 'gpd-pocket' or 'gpd-pocket2'"
+    echo "        device modifications to apply to the iso image, can be 'gpd-pocket', 'gpd-pocket2' or 'gpd-micropc'"
     echo
     echo "    -h"
     echo "        display this help and exit"
