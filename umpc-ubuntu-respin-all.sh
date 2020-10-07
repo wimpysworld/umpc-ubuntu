ad0059a1ae149fdc327711ffa43ebca8a984092c
Martin Wimpress <code@flexion.org>
2020-10-07 09:35:17 +0100

    Update respin all for 20.04.1

--- umpc-ubuntu-respin-all.sh
+++ umpc-ubuntu-respin-all.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 sudo true
-for ISO_IN in ubuntu-mate-19.10-desktop-amd64.iso; do
+for ISO_IN in ubuntu-mate-20.04.1-desktop-amd64.iso; do
   for UMPC in gpd-pocket gpd-pocket2 gpd-micropc gpd-p2-max topjoy-falcon; do
-    sudo ./umpc-ubuntu-respin.sh -d ${UMPC} ${ISO_IN}
+    sudo ./umpc-ubuntu-respin.sh -d ${UMPC} "${ISO_IN}"
     if [ -e ~/Scripts/mate/sign_image.sh ]; then
       ISO_OUT=$(basename "${ISO_IN}" | sed "s/\.iso/-${UMPC}\.iso/")
-      ~/Scripts/mate/sign_image.sh ${ISO_OUT}
+      ~/Scripts/mate/sign_image.sh "${ISO_OUT}"
     fi
   done
 done
