005adae8f03e98747ba790201c739e8cb143682e
Martin Wimpress <code@flexion.org>
2022-01-21 12:33:06 +0000

    Refactor respin-all so it doesn't respin existing rebuilds

--- umpc-ubuntu-respin-all.sh
+++ umpc-ubuntu-respin-all.sh
@@ -2,11 +2,17 @@
 
 sudo true
 for ISO_IN in ubuntu-mate-20.04.3-desktop-amd64.iso ubuntu-mate-21.10-desktop-amd64.iso; do
-  for UMPC in gpd-pocket gpd-pocket2 gpd-pocket3 gpd-micropc gpd-p2-max gpd-win-max topjoy-falcon; do
-    sudo ./umpc-ubuntu-respin.sh -d ${UMPC} "${ISO_IN}"
-    if [ -e ~/Scripts/mate/sign_image.sh ]; then
-      ISO_OUT=$(basename "${ISO_IN}" | sed "s/\.iso/-${UMPC}\.iso/")
-      ~/Scripts/mate/sign_image.sh "${ISO_OUT}"
+  for UMPC in gpd-pocket gpd-pocket2 gpd-pocket3 gpd-micropc gpd-p2-max gpd-win2 gpd-win-max topjoy-falcon; do
+    echo "Making ${ISO_IN} for ${UMPC}"
+    ISO_OUT=$(basename "${ISO_IN}" | sed "s/\.iso/-${UMPC}\.iso/")
+    if [ ! -e "${ISO_OUT}" ]; then
+      sudo ./umpc-ubuntu-respin.sh -d ${UMPC} "${ISO_IN}"
+      if [ -x "${HOME}/Scripts/mate/sign_image.sh" ]; then
+        "${HOME}"/Scripts/mate/sign_image.sh "${ISO_OUT}"
+      fi
+    else
+      echo " - ${ISO_OUT} is already built."
     fi
   done
+  echo
 done
