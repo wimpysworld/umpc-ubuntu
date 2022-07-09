4d8b6471998735f83e92747757b9ce525a60ca5c
Josh Santos <josh@omnidapps.com>
2022-07-08 20:59:19 -0400

    For GPD Pocket 3, matrix is incorrect

--- data/umpc-display-rotate.c
+++ data/umpc-display-rotate.c
@@ -34,10 +34,10 @@ static const char *orientations[] = {
 };
 
 static const char *transforms[]  = {
+  "0 -1 1 1 0 0 0 0 1",
+  "0 1 0 -1 0 1 0 0 1",
   "1 0 0 0 1 0 0 0 1",
   "-1 0 1 0 -1 1 0 0 1",
-  "0 1 0 -1 0 1 0 0 1",
-  "0 -1 1 1 0 0 0 0 1",
   NULL
 };
 
